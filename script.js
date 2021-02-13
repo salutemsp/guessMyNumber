64fcc9b93e140dac73b65a808b12c4092a879f3c
Sean Pierre Salutem <77522082+salutemsp@users.noreply.github.com>
2021-02-13 21:09:29 +0800

    uploading files

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+const message = document.querySelector('.message');
+const check = document.querySelector('.check');
+let score = 20;
+let hs = 0;
+let secretNumber = Math.trunc(
+  Math.random(document.querySelector('.number').value) * 20 + 1
+);
+
+const setMessage = text => {
+  {
+    return (message.textContent = text);
+  }
+};
+
+const setScore = function (score) {
+  return (document.querySelector('.score').textContent = score);
+};
+
+//set the highscore
+const highScore = function () {
+  if (score > hs) {
+    hs = score;
+    document.querySelector('.highscore').textContent = hs;
+  }
+};
+
+const setBGcolor = color =>
+  (document.querySelector('body').style.backgroundColor = color);
+
+check.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // if there is no input
+  if (!guess) {
+    message.textContent = 'Guess a Number ';
+
+    // if you win
+  } else if (guess === secretNumber) {
+    setBGcolor('#60b347');
+    document.querySelector('.number').textContent = secretNumber;
+    highScore();
+    setMessage('🎉 You Win');
+    check.disabled = true;
+
+    //if guess is different
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      score--;
+      setScore(score);
+      guess > secretNumber ? setMessage('Too high') : setMessage('Too Low');
+    } else {
+      setScore(0);
+      setMessage('Game over');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  setBGcolor('#222');
+  score = 20;
+  setScore(score);
+  const guess = document.querySelector('.guess');
+  guess.value = '';
+  setMessage('Start Guessing ...');
+  document.querySelector('.number').textContent = '?';
+  check.disabled = false;
+  secretNumber = Math.trunc(
+    Math.random(document.querySelector('.number').value) * 20
+  );
+});
